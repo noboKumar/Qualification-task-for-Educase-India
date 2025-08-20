83294295585d2efee58f10c46e7ecbe718772856
noboKumar <nobokumar227@gmail.com>
2025-08-20 20:19:13 +0600

    setup react-router

---
+++ src/Components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to PopX</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+      <button>Create Account</button>
+      <button>Already Registered? Login</button>
+    </div>
+  );
+};
+
+export default Home;

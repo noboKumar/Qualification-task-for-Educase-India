1f1295d9ad3f3a31b8f3e0b96c7f2072902e96bd
noboKumar <nobokumar227@gmail.com>
2025-08-20 20:43:37 +0600

    add home page design

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./Components/Home";
 function App() {
   return (
     <div className="bg-gray-200">
-      <div className="flex justify-center items-center min-h-screen w-3/12 mx-auto px-5 bg-white">
+      <div className="flex justify-center items-center min-h-screen max-w-[375px] mx-auto px-5 bg-white rubik-font">
         <Outlet></Outlet>
       </div>
     </div>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,11 +2,24 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to PopX</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
-      <button>Create Account</button>
-      <button>Already Registered? Login</button>
+    <div className="flex flex-col justify-between min-h-screen py-5">
+      <div></div>
+      <div className="space-y-7">
+        <div>
+          <h1 className="text-[28px] font-medium">Welcome to PopX</h1>
+          <p className="text-lg text-black/60">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          </p>
+        </div>
+        <div className="flex flex-col gap-2.5 font-medium">
+          <button className="bg-[#6C25FF] py-3 text-white rounded-[6px] cursor-pointer">
+            Create Account
+          </button>
+          <button className="bg-[#6C25FF4B] py-3 rounded-[6px] cursor-pointer">
+            Already Registered? Login
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
